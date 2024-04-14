01fc35af5b8ab61234f5530aba1d435451fd40e7
nyo-evil <csalbin86@gmail.com>
2024-04-14 09:19:22 -0400

    14-04-2024 contacto terminado

--- index.js
+++ index.js
@@ -20,3 +20,41 @@
     menu_btn_close.classList.toggle('none');
   })
 })(document)
+
+((dc) => {
+  const $form = dc.querySelector('contact-form');
+  const $loader = dc.querySelector('contact-form-loader');
+  const $response = dc.querySelector('contact-form-response');
+
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    $loader.classList.remove('none');
+
+    fetch('https://formsubmit.co/ajax/csalbin86@gmail.com', {
+      // El método POST es una forma de enviar datos desde un formulario web a un servidor mediante el protocolo HTTP. Los datos no son 
+      // visibles en la dirección URL, a diferencia del método GET, que anexa los datos como cadenas de consulta.
+      method: 'POST',
+
+      // La información a enviar, el FormData mandara a la petición al servicio que estamos utilizando.
+      body: new FormData(e.target)
+    })
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      console.log(json);
+      location.hash = '#gracias';
+      $form.reset();
+    })
+    .catch((error) => {
+      console.log(error);
+      let message = error.statusText || 'Ocurrio un error al enviar';
+      $loader.querySelector('h3').innerHTML = `Error ${error.status}: ${message}`; 
+      
+    }).finally(() => {
+      $loader.classList.add('none');
+
+      setTimeout(() => {
+        location.hash = '#close';
+      }, 3000)
+    });
+  });
+})(document)
